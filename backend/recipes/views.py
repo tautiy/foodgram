27ecae2043029a1d69a1e89cba6cc88315516473
Alexander Tautiev <tautiyalex@gmail.com>
2026-07-31 16:55:59 +0300

    Fix PEP8: remove unused imports

--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -1,6 +1,5 @@
 from django.db.models import Sum
 from django.http import HttpResponse
-from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters, permissions, status, viewsets
 from rest_framework.decorators import action
