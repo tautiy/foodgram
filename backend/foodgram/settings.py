c3166b56314bc1b5bb9e8f0165277bee57b4580d
Alexander Tautiev <tautiyalex@gmail.com>
2026-08-10 13:03:14 +0300

    Fix DEFAULT_PAGINATION_CLASS to use LimitPageNumberPagination

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -94,7 +94,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
     ],
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'DEFAULT_PAGINATION_CLASS': 'api.pagination.LimitPageNumberPagination',
     'PAGE_SIZE': 6,
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend',
